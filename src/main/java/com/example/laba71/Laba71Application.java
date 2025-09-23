b53bd96fb10b65446db8a3005f0eb4931fcaf94f
Алексей <khvan_leha@mail.ru>
2025-09-23 17:04:27 +0600

    Initial commit

---
+++ src/main/java/com/example/laba71/Laba71Application.java
@@ -0,0 +1,13 @@
+package com.example.laba71;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba71Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laba71Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/laba71/Laba71ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laba71;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba71ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
