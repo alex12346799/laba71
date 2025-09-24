ba22a07e36a7ad4be939b872f5c0304c9d70097e
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-24 18:57:09 +0600

    images

--- src/main/java/com/example/laba71/controller/MainController.java
+++ src/main/java/com/example/laba71/controller/MainController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-
 @Controller
 @RequiredArgsConstructor
 public class MainController {
