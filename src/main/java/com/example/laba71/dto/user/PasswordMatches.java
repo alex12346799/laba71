efca81dc72c5dcf7a95491bb14b2335d810f685a
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2025-09-24 12:42:22 +0600

    create PasswordMatches

---
+++ src/main/java/com/example/laba71/dto/user/PasswordMatches.java
@@ -0,0 +1,16 @@
+package com.example.laba71.dto.user;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+public @interface PasswordMatches {
+    String message() default "Пароли не совпадают";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
